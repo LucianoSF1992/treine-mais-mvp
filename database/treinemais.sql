559a7fd18bcbd38cfab3cc9c655c0d81c8a11775
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-02-05 07:51:35 -0300

    feat: modelagem e estrutura inicial do banco de dados MySQL

---
+++ database/treinemais.sql
@@ -0,0 +1,73 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: treinemais
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `alunos`
+--
+
+LOCK TABLES `alunos` WRITE;
+/*!40000 ALTER TABLE `alunos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alunos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `exercicio_conclusoes`
+--
+
+LOCK TABLES `exercicio_conclusoes` WRITE;
+/*!40000 ALTER TABLE `exercicio_conclusoes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exercicio_conclusoes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `exercicios`
+--
+
+LOCK TABLES `exercicios` WRITE;
+/*!40000 ALTER TABLE `exercicios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exercicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `treinos`
+--
+
+LOCK TABLES `treinos` WRITE;
+/*!40000 ALTER TABLE `treinos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `treinos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Administrador do Sistema','administratorappacademia@academia.com','admin','ADMIN','2026-02-05 10:10:39');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-05  7:47:51
