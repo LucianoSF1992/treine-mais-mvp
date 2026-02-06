ef5acc841ab2a49c0eebea3d430b36863c991aed
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-02-06 07:06:57 -0300

    fix: adicionar relacionamento User no model Aluno

--- TreineMais.Api/Models/Aluno.cs
+++ TreineMais.Api/Models/Aluno.cs
@@ -3,8 +3,14 @@ namespace TreineMais.Api.Models
     public class Aluno
     {
         public int Id { get; set; }
+
+        // FK para User
         public int UserId { get; set; }
+        public User? User { get; set; }
+
+        // FK para Instrutor (User)
         public int InstrutorId { get; set; }
+
         public int? Idade { get; set; }
         public string? Objetivo { get; set; }
     }
