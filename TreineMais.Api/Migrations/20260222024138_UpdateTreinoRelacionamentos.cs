1bfebc8379431292343ae03e1ccaa6e0d83c50d3
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-02-21 23:48:27 -0300

    feat: update treino relationships and database migrations

---
+++ TreineMais.Api/Migrations/20260222024138_UpdateTreinoRelacionamentos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TreineMais.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTreinoRelacionamentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "Treino");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AlunoId",
+                table: "Treino",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ExercicioTreinos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TreinoId = table.Column<int>(type: "int", nullable: false),
+                    ExercicioId = table.Column<int>(type: "int", nullable: false),
+                    Series = table.Column<int>(type: "int", nullable: false),
+                    Repeticoes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExercicioTreinos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExercicioTreinos_Exercicios_ExercicioId",
+                        column: x => x.ExercicioId,
+                        principalTable: "Exercicios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExercicioTreinos_Treino_TreinoId",
+                        column: x => x.TreinoId,
+                        principalTable: "Treino",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExercicioTreinos_ExercicioId",
+                table: "ExercicioTreinos",
+                column: "ExercicioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExercicioTreinos_TreinoId",
+                table: "ExercicioTreinos",
+                column: "TreinoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExercicioTreinos");
+
+            migrationBuilder.DropColumn(
+                name: "AlunoId",
+                table: "Treino");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Treino",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
